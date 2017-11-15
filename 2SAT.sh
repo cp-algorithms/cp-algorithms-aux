53317feb02d10fb6c7cbcbf8c0f9c3bc6f24b7b8
Jakob Kogler <jakob.kogler@gmail.com>
2017-11-15 22:59:20 +0100

    Resize images to be of same size

--- 2SAT.sh
+++ 2SAT.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-convert -density 300 2SAT.pdf -quality 100 -resize x380 -depth 4 2SAT.png
+convert -density 300 2SAT.pdf -quality 100 -resize 732 -depth 4 2SAT.png
--- 2SAT_SCC.sh
+++ 2SAT_SCC.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-convert -density 300 2SAT_SCC.pdf -quality 100 -resize x380 -depth 4 2SAT_SCC.png
+convert -density 300 2SAT_SCC.pdf -quality 100 -resize 732 -depth 4 2SAT_SCC.png

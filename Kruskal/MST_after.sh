40bda007cb20f7b3af79c2d892a8ebaaeba99283
Jakob Kogler <jakob.kogler@gmail.com>
2018-01-26 13:54:29 +0100

    Images for MST Kruskal

---
+++ Kruskal/MST_after.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+convert -density 300 MST_after.pdf -quality 100 -resize 350 -depth 4 MST_after.png
---
+++ Kruskal/MST_before.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+convert -density 300 MST_before.pdf -quality 100 -resize 350 -depth 4 MST_before.png

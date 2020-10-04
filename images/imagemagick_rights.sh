31d11a37fdedb9a2051706c51f56ee47e7923d6e
Jakob Kogler <jakob.kogler@gmail.com>
2020-10-04 11:34:56 +0200

    change imagemagick policies

--- images/imagemagick_rights.sh
+++ images/imagemagick_rights.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ImageMagick has very strict read policies since version 6 (6?) and disallows reading PDFs.
 # To be able to convert PDFs to PNGs, we have to change these policy settings.
-POLICY_PATH="/etc/ImageMagick-6/policy.xml"
+POLICY_PATH="/etc/ImageMagick-7/policy.xml"
 perl -pe 's/(?<=policy domain="coder" rights=")none(?=" pattern="PDF")/read/g' $POLICY_PATH > policy.xml
-sudo mv policy.xml /etc/ImageMagick-6/policy.xml
+sudo mv policy.xml /etc/ImageMagick-7/policy.xml

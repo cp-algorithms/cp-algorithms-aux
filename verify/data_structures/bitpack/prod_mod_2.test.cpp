827ba56f34c1b40e2550c26e0c8b2912ea39201e
Oleksandr Kulkov <melfice@google.com>
2024-02-19 00:31:35 +0100

    add bitpack

---
+++ verify/data_structures/bitpack/prod_mod_2.test.cpp
@@ -0,0 +1,48 @@
+
+// @brief Matrix Product (Mod 2)
+#define PROBLEM "https://judge.yosupo.jp/problem/matrix_product_mod_2"
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,tune=native")
+#include "cp-algo/data_structures/bitpack.hpp"
+#include <bits/stdc++.h>
+
+using namespace std;
+using cp_algo::data_structures::bitpack;
+
+const int maxn = 1 << 12;
+bitpack<maxn> a[maxn], b[maxn], c[maxn];
+
+void solve() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    string row;
+    for(int i = 0; i < n; i++) {
+        cin >> row;
+        a[i] = row;
+    }
+    for(int i = 0; i < m; i++) {
+        cin >> row;
+        b[i] = row;
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(a[i][j]) {
+                c[i] ^= b[j];
+            }
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        row = c[i].to_string().substr(0, k);
+        cout << row << "\n";
+    }
+}
+
+signed main() {
+    //freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    while(t--) {
+        solve();
+    }
+}

628f0627066ef5a60f37aa1dc114909f36dd3b98
Oleksandr Kulkov <adamant.pwn@gmail.com>
2024-06-10 04:10:51 +0200

    Update rng.hpp

--- cp-algo/random/rng.hpp
+++ cp-algo/random/rng.hpp
@@ -3,11 +3,8 @@
 #include <chrono>
 #include <random>
 namespace cp_algo::random {
-    uint64_t rng() {
-        static std::mt19937_64 rng(
-            std::chrono::steady_clock::now().time_since_epoch().count()
-        );
-        return rng();
-    }
+    std::mt19937_64 rng(
+        std::chrono::steady_clock::now().time_since_epoch().count()
+    );
 }
 #endif // CP_ALGO_RANDOM_RNG_HPP

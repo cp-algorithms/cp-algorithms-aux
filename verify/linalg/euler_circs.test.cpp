3c66602817c7ab64295decbdd76fe5730673d635
Oleksandr Kulkov <adamant.pwn@gmail.com>
2024-07-03 03:25:06 +0200

    Euler circuits count test

---
+++ verify/linalg/euler_circs.test.cpp
@@ -0,0 +1,59 @@
+// @brief Counting Eulerian Circuits
+#define PROBLEM "https://judge.yosupo.jp/problem/counting_eulerian_circuits"
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("tune=native")
+#include "cp-algo/math/combinatorics.hpp"
+#include "cp-algo/linalg/matrix.hpp"
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace cp_algo::math;
+using namespace cp_algo::linalg;
+
+const int mod = 998244353;
+using base = modint<mod>;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    matrix<base> a(n);
+    int r = 0;
+    vector<int> indeg(n), outdeg(n);
+    for(int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        a[u][v] -= 1;
+        a[v][v] += 1;
+        outdeg[u]++;
+        indeg[v]++;
+        r = v;
+    }
+    a[r][r] = fact<base>(indeg[r] - 1);
+    for(int i = 0; i < n; i++) {
+        if(i == r) {
+            continue;
+        }
+        if(indeg[i] != outdeg[i]) {
+            cout << 0 << "\n";
+            return;
+        }
+        if(indeg[i] != 0) {
+            a[i] *= fact<base>(indeg[i] - 1);
+        } else {
+            a[i][i] = 1;
+        }
+        a[r][i] = a[i][r] = 0;
+    }
+    cout << a.det() << "\n";
+}
+
+signed main() {
+    //freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+        solve();
+    }
+}

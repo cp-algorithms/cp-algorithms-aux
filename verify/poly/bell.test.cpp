4a5728220ecd561b01cfde7dcee8c0321402192e
Oleksandr Kulkov <adamant.pwn@gmail.com>
2024-11-11 21:45:24 +0100

    Bell Number test

---
+++ verify/poly/bell.test.cpp
@@ -0,0 +1,28 @@
+// @brief Bell Number
+#define PROBLEM "https://judge.yosupo.jp/problem/bell_number"
+#pragma GCC optimize("Ofast,unroll-loops")
+#include "cp-algo/math/poly.hpp"
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace cp_algo::math;
+
+const int mod = 998244353;
+using base = modint<mod>;
+using polyn = poly_t<base>;
+
+void solve() {
+    int n;
+    cin >> n;
+    (polyn::expx(n+1) - polyn(1)).exp_inplace(n+1).invborel().print(n+1);
+}
+
+signed main() {
+    //freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    while(t--) {
+        solve();
+    }
+}

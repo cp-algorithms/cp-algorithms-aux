3280008e5dde414c30ea62023cb6a9c46d58e5fb
Oleksandr Kulkov <adamant.pwn@gmail.com>
2024-11-12 21:38:12 +0100

    Update

--- cp-algo/number_theory/factorize.hpp
+++ cp-algo/number_theory/factorize.hpp
@@ -5,37 +5,38 @@
 #include <generator>
 namespace cp_algo::math {
     // https://en.wikipedia.org/wiki/Pollard%27s_rho_algorithm
+
+    auto proper_divisor(uint64_t m) {
+        using base = dynamic_modint<>;
+        return m % 2 == 0 ? 2 : base::with_mod(m, [&]() {
+            base t = random::rng();
+            auto f = [&](auto x) {
+                return x * x + t;
+            };
+            base x, y;
+            base g = 1;
+            while(g == 1) {
+                for(int i = 0; i < 64; i++) {
+                    x = f(x);
+                    y = f(f(y));
+                    if(x == y) [[unlikely]] {
+                        t = random::rng();
+                        x = y = 0;
+                    } else {
+                        base t = g * (x - y);
+                        g = t == 0 ? g : t;
+                    }
+                }
+                g = std::gcd(g.getr(), m);
+            }
+            return g.getr();
+        });
+    }
     std::generator<uint64_t> factorize(uint64_t m) {
-        if(m % 2 == 0) {
-            co_yield std::ranges::elements_of(factorize(m / 2));
-            co_yield 2;
-        } else if(is_prime(m)) {
+        if(is_prime(m)) {
             co_yield m;
         } else if(m > 1) {
-            using base = dynamic_modint<int64_t>;
-            auto g = base::with_mod(m, [&]() {
-                base t = random::rng();
-                auto f = [&](auto x) {
-                    return x * x + t;
-                };
-                base x, y;
-                base g = 1;
-                while(g == 1) {
-                    for(int i = 0; i < 64; i++) {
-                        x = f(x);
-                        y = f(f(y));
-                        if(x == y) [[unlikely]] {
-                            t = random::rng();
-                            x = y = 0;
-                        } else {
-                            base t = g * (x - y);
-                            g = t == 0 ? g : t;
-                        }
-                    }
-                    g = std::gcd(g.getr(), m);
-                }
-                return g.getr();
-            });
+            auto g = proper_divisor(m);
             co_yield std::ranges::elements_of(factorize(g));
             co_yield std::ranges::elements_of(factorize(m / g));
         }

1e86f971c37b9758c4b69f7993478252eaaaee5b
Oleksandr Kulkov <adamant.pwn@gmail.com>
2025-04-24 16:05:27 +0200

    Vectorize filling ans

--- verify/poly/wildcard.test.cpp
+++ verify/poly/wildcard.test.cpp
@@ -11,6 +11,7 @@ using namespace cp_algo::math;
 
 using fft::ftype;
 using fft::point;
+using fft::vftype;
 using fft::cvector;
 
 void semicorr(auto &a, auto &b) {
@@ -20,11 +21,28 @@ void semicorr(auto &a, auto &b) {
     a.ifft();
 }
 
-auto is_integer = [](point a) {
+vftype abs(vftype a) {
+    return a < 0 ? -a : a;
+}
+
+using v4di [[gnu::vector_size(32)]] = long;
+
+auto round(vftype a) {
+    return __builtin_convertvector(__builtin_convertvector(a < 0 ? a - 0.5 : a + 0.5, v4di), vftype);
+}
+
+void print(auto r) {
+    for(int z = 0; z < 4; z++) {
+        cout << r[z] << ' ';
+    }
+    cout << endl;
+}
+
+auto is_integer(auto a) {
     static const double eps = 1e-8;
     return abs(imag(a)) < eps
         && abs(real(a) - round(real(a))) < eps;
-};
+}
 
 string matches(string const& A, string const& B, char wild = '*') {
     static const int sigma = 26;
@@ -48,12 +66,17 @@ string matches(string const& A, string const& B, char wild = '*') {
     }
     cp_algo::checkpoint("cvector fill");
     semicorr(P[0], P[1]);
-    string ans(size(A) - size(B) + 1, '0');
-    for(size_t j = 0; j < size(ans); j++) {
-        ans[j] = '0' + is_integer(P[0].get(size(B) - 1 + j));
+    string ans(size(P[0]), '0');
+    auto start = (size(B) - 1) / fft::flen * fft::flen;
+    for(size_t j = start; j < size(ans); j += fft::flen) {
+        auto r = P[0].at(j);
+        auto check = is_integer(r);
+        for(int z = 0; z < 4; z++) {
+            ans[j + z] ^= (bool)check[z];
+        }
     }
     cp_algo::checkpoint("fill answer");
-    return ans;
+    return ans.substr(size(B) - 1, size(A) - size(B) + 1);
 }
 
 void solve() {

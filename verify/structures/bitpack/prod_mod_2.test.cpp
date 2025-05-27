2153a8e434b7fced3bf51aeaeaed3de4cbe7d606
Oleksandr Kulkov <adamant.pwn@gmail.com>
2025-05-27 23:52:24 +0200

    update prod_mod_2

--- verify/structures/bitpack/prod_mod_2.test.cpp
+++ verify/structures/bitpack/prod_mod_2.test.cpp
@@ -1,8 +1,9 @@
-
 // @brief Matrix Product (Mod 2)
 #define PROBLEM "https://judge.yosupo.jp/problem/matrix_product_mod_2"
 #pragma GCC optimize("Ofast,unroll-loops")
+#define CP_ALGO_CHECKPOINT
 #include "cp-algo/structures/bitpack.hpp"
+#include "cp-algo/util/checkpoint.hpp"
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -25,6 +26,7 @@ void process_precalc() {
 }
 
 void solve() {
+    cp_algo::checkpoint("init");
     int n, m, k;
     cin >> n >> m >> k;
     string row;
@@ -36,7 +38,9 @@ void solve() {
         cin >> row;
         b[i] = row;
     }
+    cp_algo::checkpoint("read");
     process_precalc();
+    cp_algo::checkpoint("precalc");
     for(int j = 0; j < m; j += 64) {
         for(int z = 0; z < 64 / K; z++) {
             for(int i = 0; i < n; i++) {
@@ -44,10 +48,13 @@ void solve() {
             }
         }
     }
+    cp_algo::checkpoint("mul");
     for(int i = 0; i < n; i++) {
         row = c[i].to_string().substr(0, k);
         cout << row << "\n";
     }
+    cp_algo::checkpoint("write");
+    cp_algo::checkpoint<1>();
 }
 
 signed main() {

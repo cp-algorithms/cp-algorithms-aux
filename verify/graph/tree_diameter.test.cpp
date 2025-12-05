1be5abcba4cb9520871fd0222ca822a83a2bed6e
Oleksandr Kulkov <adamant.pwn@gmail.com>
2025-12-05 16:00:26 +0100

    xor_dfs, tree diameter

---
+++ verify/graph/tree_diameter.test.cpp
@@ -0,0 +1,36 @@
+// @brief Tree Diameter
+#define PROBLEM "https://judge.yosupo.jp/problem/tree_diameter"
+#pragma GCC optimize("Ofast,unroll-loops")
+#include <iostream>
+#include "blazingio/blazingio.min.hpp"
+#include "cp-algo/graph/tree_diameter.hpp"
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace cp_algo::graph;
+
+void solve() {
+    int n;
+    cin >> n;
+    weighted_graph g(n);
+    g.read_edges(n - 1);
+    auto [d, v, path] = tree_diameter(g);
+    cout << d << ' ' << size(path) + 1 << '\n';
+    cout << v;
+    for(auto e: path) {
+        v = g.edge(e).traverse(v);
+        cout << ' ' << v;
+    }
+    cout << "\n";
+}
+
+signed main() {
+    //freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    //cin >> t;
+    while(t--) {
+        solve();
+    }
+}

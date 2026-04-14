148165bf9d1b869398e0f94041bdbe949efe8520
Akram <akram042006@gmail.com>
2026-04-13 17:22:02 -0700

    revert cp-algo/min/*

--- cp-algo/min/util/generator.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef CP_ALGO_UTIL_GENERATOR_HPP
-#define CP_ALGO_UTIL_GENERATOR_HPP
-#include "big_alloc.hpp"
-#include <generator>
-namespace cp_algo{template<typename Ref,typename V=void>using big_generator=std::generator<Ref,V,big_alloc<std::byte>>;}namespace std::ranges{template<typename Ref,typename V>elements_of(cp_algo::big_generator<Ref,V>&&)->elements_of<cp_algo::big_generator<Ref,V>&&,cp_algo::big_alloc<std::byte>>;}
-#endif
